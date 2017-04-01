0fb9ebc5168a6deb02d8daacfb3cef27bbdf7422
calberca <cesalberca@gmail.com>
2017-04-01 16:13:09 +0200

    Changed bashrc to bash_profile

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
 dir=~/dotfiles
+olddir=~/dotfiles_old
 files=".bash_profile"
+
+# If $olddir exists then delete it
+if [ -d "$olddir" ]; then
+    rm -rf $olddir
+fi
+
+# Create a dir to move the configuration files there
+mkdir -p $olddir
 
 cd $dir
 
 for file in $files; do
+    mv ~/$file ~/$olddir/
     echo "Creating symlink to $file in home directory."		    
     ln -s $dir/$file ~/$file		
 done
