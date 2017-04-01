deb22738eaa59a8c244faa55bedae3e3b5d634d9
calberca <cesalberca@gmail.com>
2017-04-01 15:47:33 +0200

    Working as intended

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-ln -s ".bash_profile" ~/.bash_profile
+dir=~/dotfiles
+files=".bash_profile"
+
+cd $dir
+
+for file in $files; do
+    echo "Creating symlink to $file in home directory."		    
+    ln -s $dir/$file ~/$file		
+done
 
 source ~/.bash_profile
