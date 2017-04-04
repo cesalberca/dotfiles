50be6e0600643b3ff76afe33e71f66b4a8a68850
calberca <cesalberca@gmail.com>
2017-04-04 14:43:04 +0200

    Updated installation

--- install.sh
+++ install.sh
@@ -4,20 +4,16 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 files=".bash_profile .zshrc"
 
-# If $olddir exists then delete it
-if [ -d "$olddir" ]; then
-    rm -rf $olddir
-fi
-
 # Create a dir to move the configuration files there
+rm -ri $olddir
 mkdir -p $olddir
 
 cd $dir
 
 for file in $files; do
     mv ~/$file ~/$olddir/
-    echo "Creating symlink to $file in home directory."		    
-    ln -s $dir/$file ~/$file		
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
 done
 
 source ~/.bash_profile
