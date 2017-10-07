3804389d521874379c03f583ee6f6d251ee4b734
César Alberca <calberca@Autentias-MacBook-Pro.local>
2017-10-07 15:11:27 +0200

    Add homebrew installer and oh my zsh

--- install.sh
+++ install.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-dir=~/dotfiles
-olddir=~/dotfiles_old
-files=".zshrc"
+# Install Hombrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
 
-mkdir -p $olddir
+# Use hammerspoon configuration
+curl -o ~/.hammerspoon/init.lua https://gist.githubusercontent.com/JavierSA/afcfa5ccaceabcb6ab84/raw/e00f6cf3c4890cb91c0e79a824b5b35a3fedec8b/init.lua
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change default shell
+chsh -s /bin/zsh
+
+# Configure profile
+dir=~/dotfiles
+files=".zshrc"
 
 # Create a dir to move the configuration files there
 cd $dir
 
 for file in $files; do
-    mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
