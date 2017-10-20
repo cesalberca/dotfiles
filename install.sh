26fb324d2ac4e83bf032872f2e09827fc1d8d9fa
César Alberca <cesalberca@gmail.com>
2017-10-20 15:48:43 +0200

    Review installation process

--- install.sh
+++ install.sh
@@ -5,19 +5,10 @@
 brew bundle
 
 # Use hammerspoon configuration
-curl -o ~/.hammerspoon/init.lua https://gist.githubusercontent.com/JavierSA/afcfa5ccaceabcb6ab84/raw/e00f6cf3c4890cb91c0e79a824b5b35a3fedec8b/init.lua
-
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Change default shell
-chsh -s /bin/zsh
+curl -o ~/.hammerspoon/init.lua https://gist.githubusercontent.com/cesalberca/bf06aa9c10b3cfa648284e2ffb7d09c2/raw/31d238ee7b47d06e6cb01f1a803636a2c2a300a4/init.lua
 
 # Install Powerlevel9k theme
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-# Download meslo font
-curl -o ~/Library/Fonts/Meslo\ LG\ M\ Regular\ for\ Powerline.ttf https://github.com/powerline/fonts/blob/master/Meslo%20Slashed/Meslo%20LG%20M%20Regular%20for%20Powerline.ttf
 
 # Install VSCode extensions
 code --install-extension msjsdiag.debugger-for-chrome
@@ -38,8 +29,14 @@ touch ~/.hushlogin
 # Configure fzf and install keybidings
 $(brew --prefix)/opt/fzf/install
 
-# Change MacOS configuration
-source .macos
+# Install sdkman
+curl -s "https://get.sdkman.io" | bash
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change default shell
+chsh -s /bin/zsh
 
 # Configure profile
 dir=~/dotfiles
@@ -50,10 +47,9 @@ cd $dir
 
 for file in $files; do
     echo "Creating symlink to $file in home directory."
+    rm ~/$file
     ln -s $dir/$file ~/$file
 done
 
-# Install sdkman
-curl -s "https://get.sdkman.io" | bash
-
-source ~/.zshrc
+# Change MacOS configuration
+source .macos
